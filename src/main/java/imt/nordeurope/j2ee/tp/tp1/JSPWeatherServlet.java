29be92efb26e977948db3621fce0bc371282ab9e
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-12-13 17:52:29 +0100

    Create Weather JSP

---
+++ src/main/java/imt/nordeurope/j2ee/tp/tp1/JSPWeatherServlet.java
@@ -0,0 +1,26 @@
+package imt.nordeurope.j2ee.tp.tp1;
+
+        import javax.servlet.RequestDispatcher;
+        import javax.servlet.ServletException;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+
+@WebServlet(name = "JSPWeatherServlet", value = "/JSPWeather")
+
+public class JSPWeatherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Weather.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/Weather.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSP - Température</title>
+</head>
+<body>
+<h1>Température</h1>
+<br/>
+
+</body>
+</html>
