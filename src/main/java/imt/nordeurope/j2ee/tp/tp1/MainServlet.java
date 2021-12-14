78ff5ecae9fb7ffc8f2fe67210ca7f981232a808
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-12-14 15:18:15 +0100

    AJAX Exo : create classes

---
+++ src/main/java/imt/nordeurope/j2ee/tp/tp1/MainServlet.java
@@ -0,0 +1,20 @@
+package imt.nordeurope.j2ee.tp.tp1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MainServlet", value = "/MainServlet")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Main.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/Main.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: guill
+  Date: 14/12/2021
+  Time: 15:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+</head>
+<>
+    <div id="quote">
+        <hr>
+        <p>Hello </p>
+        <hr/>
+    </div>
+    <form>
+        <input type="text" id="name" value="Stephane">
+        <input type="submit" id="replaceContent" value="Refresh local"></input>
+    </form>
+</body>
+</html>
